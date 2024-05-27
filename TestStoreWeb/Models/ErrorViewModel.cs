c3f47a12542aa17b2ad4cde5a51f3143faa5543c
DucTriLeCao <trilcdse182667@fpt.edu.vn>
2024-05-27 10:37:48 +0700

    Add project files.

---
+++ TestStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
